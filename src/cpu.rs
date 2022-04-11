10d729e3b893baa8b42f15a6e5bf76f0c1955d18
Natsuki YOKOTA <natsuki.yokota2@persol.co.jp>
2022-04-11 13:20:46 +0900

    Update: impl CPL

--- src/cpu.rs
+++ src/cpu.rs
@@ -201,6 +201,7 @@ impl Cpu {
         match opcode {
             Opcode { cb_prefix: false, code: res } => {
                 match res {
+                    0x2F => self.cpl(),
                     0x27 => self.decimal_adjust_accumlator(),
                     0x3B => self.dec_3B(),
                     0x2B => self.dec_2B(),
@@ -530,6 +531,19 @@ impl Cpu {
     }
 
     // region: inst
+    #[allow(dead_code)]
+    fn cpl(&mut self) -> Result<u8> {
+        let val = self.A;
+        let fliped_val = val ^ 0xFF;
+        self.A = fliped_val;
+
+        let z = self.get_zero_flag();
+        let c = self.get_carry_flag();
+        self.set_flag(z, true, true, c);
+
+        Ok(4)
+    }
+
     #[allow(dead_code)]
     fn decimal_adjust_accumlator(&mut self) -> Result<u8> {
         let mut val = self.A;
