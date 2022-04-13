1c83ef91a3460a2a678e12e4ccd3808fc968e1ef
横田 夏樹（346549） <ty346549@pptnoMacBook-Pro.local>
2022-04-14 08:53:38 +0900

    Update: add winit, winit-input-helper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
 mod rom;
 mod mbc;
 mod bus;
@@ -5,5 +9,20 @@ mod cpu;
 mod ppu;
 
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("My Game Boy")
+        .build(&event_loop)
+        .unwrap();
+    
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => ()
+        }
+    })
 }
