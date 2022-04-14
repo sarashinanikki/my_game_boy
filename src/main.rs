c070ff92b4156762aa968777fad5b26679edff6d
横田 夏樹（346549） <ty346549@pptnoMacBook-Pro.local>
2022-04-14 09:39:21 +0900

    Update: impl pixels & winit example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+use winit::dpi::LogicalSize;
+use pixels::{Error, Pixels, SurfaceTexture};
 use winit_input_helper::WinitInputHelper;
 
 mod rom;
@@ -14,8 +16,14 @@ fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("My Game Boy")
+        .with_inner_size(LogicalSize::new(320, 240))
+        .with_min_inner_size(LogicalSize::new(320, 240))
         .build(&event_loop)
         .unwrap();
+
+    let window_size = window.inner_size();
+    let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+    let mut pixels = Pixels::new(320, 240, surface_texture).unwrap();
     
     event_loop.run(move |event, _, control_flow| {
         if input.update(&event) {
@@ -23,12 +31,32 @@ fn main() {
                 *control_flow = ControlFlow::Exit;
                 return;
             }
+        }
 
-            let mouse_diff = input.mouse_diff();
-            if mouse_diff != (0.0, 0.0) {
-                println!("Diff is: {:?}", mouse_diff);
-                println!("Mouse position is: {:?}", input.mouse());
+        if let Event::RedrawRequested(_) = event {
+            draw(pixels.get_frame());
+            if pixels.render().is_err() {
+                *control_flow = ControlFlow::Exit;
+                return;
             }
         }
     })
+}
+
+fn draw(frame: &mut [u8]) {
+    for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+        let x = (i % 320 as usize) as i16;
+        let y = (i / 320 as usize) as i16;
+
+        let inside = x >= 10 && x < 110
+            && y > 20 && y < 120;
+
+        let rgba = if inside {
+            [0x5e, 0x99, 0x39, 0xff]
+        } else {
+            [0x48, 0xb2, 0xe8, 0xff]
+        };
+
+        pixel.copy_from_slice(&rgba);
+    }
 }
