d1ba9bb9f2263aa6a2f185dd7e0c339a268581b8
sarashin <sarashina.nikki65@gmail.com>
2022-07-23 20:39:51 +0900

    Update: MBC5を実装

--- src/mbc.rs
+++ src/mbc.rs
@@ -32,6 +32,16 @@ pub struct Mbc1 {
     pub ram: [u8; 0x8000]
 }
 
+pub struct Mbc5 {
+    pub mbc_type: u8,
+    pub rom: Rom,
+    pub is_external_ram_enable: bool,
+    pub rom_bank_number_low: u8,
+    pub rom_bank_number_high: bool,
+    pub ram_bank_number: u8,
+    pub ram: [u8; 0x20000]
+}
+
 impl Mbc for NoMbc {
     fn read_rom(&self, address: u16) -> Result<u8> {
         let ret = self.rom.data[address as usize];
@@ -158,3 +168,65 @@ impl Mbc for Mbc1 {
         Ok(())
     }
 }
+
+impl Mbc for Mbc5 {
+    fn read_rom(&self, raw_address: u16) -> Result<u8> {
+        match raw_address {
+            0x0000..=0x3FFF => Ok(self.rom.data[raw_address as usize]),
+            0x4000..=0x7FFF => {
+                let mut rom_bank_number = self.rom_bank_number_low as usize;
+                if self.rom_bank_number_high {
+                    rom_bank_number += 1 << 8;
+                }
+                let address = 0x4000 * rom_bank_number + (raw_address as usize - 0x4000);
+                Ok(self.rom.data[address])
+            },
+            _ => bail!("Error in mbc5: invalid address")
+        }
+    }
+
+    fn read_ram(&self, raw_address: u16) -> Result<u8> {
+        if self.is_external_ram_enable {
+            let address = 0x2000 * self.ram_bank_number as usize + (raw_address as usize - 0xA000);
+            let data = self.ram[address];
+            Ok(data)
+        }
+        else {
+            Ok(0xFF)
+        }
+    }
+
+    fn write_ram(&mut self, raw_address: u16, data: u8) -> Result<()> {
+        if self.is_external_ram_enable {
+            let address = 0x2000 * self.ram_bank_number as usize + (raw_address as usize - 0xA000);
+            self.ram[address] = data;
+            Ok(())
+        }
+        else {
+            Ok(())
+        }
+    }
+
+    fn write_registers(&mut self, address: u16, data: u8) -> Result<()> {
+        match address {
+            0x0000..=0x1FFF => {
+                if data == 0x0A {
+                    self.is_external_ram_enable = true;
+                }
+                else {
+                    self.is_external_ram_enable = false;
+                }
+            },
+            0x2000..=0x2FFF => {
+                self.rom_bank_number_low = data;
+            },
+            0x3000..=0x3FFF => {
+                self.rom_bank_number_high = (data & 0x01) > 0;
+            },
+            0x4000..=0x5FFF => self.ram_bank_number = data & 0x0F,
+            _ => {}
+        }
+        Ok(())
+    }
+}
+
